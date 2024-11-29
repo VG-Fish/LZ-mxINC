b7c39ac2f12ae48bd3730b59fe0a1a5136031c3d
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-11-29 16:23:42 -0600

    added basic google login

--- client/LZ-mxINC/src/main.tsx
+++ client/LZ-mxINC/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GoogleOAuthProvider clientId="944494727715-ep7hfqopf3f1lb4an6ndkehc59kn7fkh.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
