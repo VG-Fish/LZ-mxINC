d9454b564a775ea1360bfbba87d7efe91cb1825c
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-11-29 16:49:05 -0600

    added routes

--- client/LZ-mxINC/src/App.tsx
+++ client/LZ-mxINC/src/App.tsx
@@ -1,7 +1,10 @@
 import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import { JwtPayload, jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 const App = () => {
+  const navigator = useNavigate();
+
   const onSuccess = (credentialResponse: CredentialResponse) => {
     const user: JwtPayload & { email: string } = jwtDecode(
       credentialResponse.credential ?? ""
@@ -9,7 +12,7 @@ const App = () => {
     const email = user.email;
     const org = email.split("@")[1];
     if (["lz95.org", "lz95.net"].indexOf(org) !== -1) {
-      // Go to next page
+      navigator("/store");
     }
   };
 
--- client/LZ-mxINC/src/main.tsx
+++ client/LZ-mxINC/src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  // placeholder
+  { path: "/store", element: <App /> },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="944494727715-ep7hfqopf3f1lb4an6ndkehc59kn7fkh.apps.googleusercontent.com">
-      <App />
+      <RouterProvider router={router} />
     </GoogleOAuthProvider>
   </StrictMode>
 );
