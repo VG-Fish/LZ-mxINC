84124eb080704b680566ac234aa23aaf9db14122
Abdullah Bashir <abdullahmb0007@gmail.com>
2024-12-12 21:55:42 -0600

    Home page but better???

--- client/LZ-mxINC/src/components/Home.tsx
+++ client/LZ-mxINC/src/components/Home.tsx
@@ -4,18 +4,14 @@ import UserInput from "./UserInput";
 const Home = () => {
   return (
     <div
-      className=" mt-1 mb-5 border rounded d-flex justify-content-center align-items-center container"
+      className="d-flex justify-content-center align-items-center"
       style={{
-        width: "600px",
-        height: "400px",
+        width: "100vh",
+        height: "100vh",
         backgroundImage: "linear-gradient(to bottom, #a2d2ff, #003049)",
-        top: "50%",
-        left: "50%",
-        position: "fixed",
-        transform: "translate(-50%, -50%)",
       }}
     >
-      <div className="text-center ">
+      <div className="text-center">
         <h1>
           Welcome to <b>LZ-mxINC</b>
         </h1>
