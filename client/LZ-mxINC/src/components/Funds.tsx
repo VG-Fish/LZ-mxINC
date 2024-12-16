9e844e1509970a39912461854581fa3992b60def
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-12-15 20:32:49 -0600

    added funds

--- client/LZ-mxINC/src/components/Funds.tsx
+++ client/LZ-mxINC/src/components/Funds.tsx
@@ -1,12 +1,29 @@
 import axios, { AxiosResponse } from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+interface GetUserBalanceApiResponse {
+  success: boolean;
+  message: string;
+  $numberDecimal: number;
+}
 
 const Funds = () => {
+  const [balance, setBalance] = useState(0);
   const getUserBalance = () => {
     const id = localStorage.getItem("loginId");
+    console.log(`https://lz-mxinc.onrender.com/getUserBalance:${id}`);
+    axios
+      .get(`https://lz-mxinc.onrender.com/getUserBalance:${id}`)
+      .then((response: AxiosResponse<GetUserBalanceApiResponse>) => {
+        setBalance(response.data.$numberDecimal);
+      });
   };
-  const [balance, setBalance] = useState(getUserBalance());
-  return <div className="text-end">Funds: 0</div>;
+
+  useEffect(() => {
+    getUserBalance();
+  }, [getUserBalance]);
+
+  return <div className="text-end">Funds: {balance}</div>;
 };
 
 export default Funds;
