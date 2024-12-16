5f66754af25415c571fa34c6871540cf4587d5f8
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-12-15 18:58:55 -0600

    userinput sends request

--- client/LZ-mxINC/src/components/UserInput.tsx
+++ client/LZ-mxINC/src/components/UserInput.tsx
@@ -1,4 +1,44 @@
+import { useState } from "react";
+import axios, { AxiosResponse } from "axios";
+
+interface CreateUserApiResponse {
+  success: boolean;
+  message: string;
+}
+
 function UserInput() {
+  const [inputValue, setInputValue] = useState("");
+
+  const createUser = () => {
+    console.log(inputValue);
+    const id = parseInt(inputValue, 10);
+    if (isNaN(id)) {
+      alert("Enter a valid number.");
+    }
+
+    const data = { id: id, period: 1 };
+    const jsonData = JSON.stringify(data);
+    axios
+      .post<CreateUserApiResponse>(
+        "https://lz-mxinc.onrender.com/createUser",
+        jsonData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response: AxiosResponse<CreateUserApiResponse>) => {
+        console.log(response.data.success);
+        console.log(response.data.message);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    setInputValue("");
+  };
+
   return (
     <>
       <div className="text-white text-center mt-2 mb-2">
@@ -7,6 +47,8 @@ function UserInput() {
           type="text"
           placeholder="Enter your assigned number here"
           style={{ width: "100%", height: "35px" }}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
         <button
           type="button"
@@ -15,6 +57,7 @@ function UserInput() {
             width: "100px",
             height: "50px",
           }}
+          onClick={createUser}
         >
           Sign In
         </button>
