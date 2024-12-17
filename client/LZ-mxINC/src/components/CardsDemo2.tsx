24b962169f217ab1e5eed3a81a8696c8c9c59667
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-12-16 18:48:20 -0600

    removed axios

--- client/LZ-mxINC/src/components/CardsDemo2.tsx
+++ client/LZ-mxINC/src/components/CardsDemo2.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import axios, { AxiosResponse } from "axios";
-import axios, { AxiosResponse } from "axios";
 
 import products from "../../../../products_info.json";
 import logo0 from "../assets/company_logos/Airelle Logo.png";
